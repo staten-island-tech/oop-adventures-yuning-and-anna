ff300dd0353784b96f6cb803a2595b788b24094d
AnnaLi3 <124058687+AnnaLi3@users.noreply.github.com>
2023-05-04 08:30:55 -0400

    :)

---
+++ classes_for_weapons.py
@@ -0,0 +1,10 @@
+class Weapon():
+    def __init__(self, damage):
+        self.damage = dmage
+class Armor():
+    def __init__(self, defense):
+        self.defense = defense
+class Bow():
+    def __init__(self, firepower):
+        self.firepower = firepower
+
