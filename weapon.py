5963c5ac509e1dc76825cae724dd2aa974e0396a
AnnaLi3 <124058687+AnnaLi3@users.noreply.github.com>
2023-05-05 08:28:29 -0400

    Update weapon.py

---
+++ weapon.py
@@ -0,0 +1,12 @@
+from classes_for_weapons import *
+Basic_Sword = Weapon(60)
+Leather_Armor = Armor(10)
+Hunter_Bow = Bow(150)
+
+Katana = Weapon(85)
+Crossbow = Bow(200)
+Bone_Armor = Armor(20)
+
+Valkyrie = Weapon(110)
+Piercer_Of_Shadows = Bow(250)
+Mithril_Armor = Armor(30)
