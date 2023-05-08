3aa2eb01d1344696ac25560564ae0efc1da94de8
ierhehe <ierhehe@gmail.com>
2023-05-08 08:36:45 -0400

    Create room_for_rooms.py

---
+++ room_for_rooms.py
@@ -0,0 +1,8 @@
+class room:
+    def __init__(self, skeleton, sLime, spider, orc, big_SLIME):
+        self.skeleton = skeleton
+        self.slime = sLime
+        self.spider = spider
+        self.orc = orc
+        self.big_SLIME = big_SLIME
+        
