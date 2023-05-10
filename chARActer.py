7207d39db6709d06ccc6ea4556b29d3e3ff85d53
ierhehe <ierhehe@gmail.com>
2023-05-10 08:33:34 -0400

    :()

---
+++ chARActer.py
@@ -0,0 +1,8 @@
+ara_ara = input("WHat do you want to name your character?")
+
+class ara_ara():
+    def __init__(self, damage, health, defense):
+        self.damage = damage
+        self.health = health 
+        self.defense = defense
+
