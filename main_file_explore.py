3ae9d28892d857cd3d72a2b86d261336516f8986
ierhehe <ierhehe@gmail.com>
2023-05-12 08:38:44 -0400

    F

--- main_file_explore.py
+++ main_file_explore.py
@@ -4,10 +4,12 @@ from roms import *
 
 from chARActer import *
 
-def attacking(attacker, defender, move):
-    damage = attacker["move"][move]
-    defender["defender"] = max(0, defender["health"] - damage)
-    print(f"{attacker['name']} used {move} and dealt {damage} damage to {defender['name']}")
+def 
+
+def attacking(ara_ara, monster, move):
+    damage = ara_ara["move"][move]
+    monster["defender"] = max(0, monster["health"] - damage)
+    print(f"{ara_ara['name']} used {move} and dealt {damage} damage to {monster['name']}")
 
 def starting():
     beginning = input("Type start_adventure when are ready to begin ")
