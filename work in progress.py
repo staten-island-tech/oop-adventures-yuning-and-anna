44a8d2b082c72c148f533de758620edd6dac9eda
ierhehe <ierhehe@gmail.com>
2023-05-18 08:26:31 -0400

    dkgf

--- work in progress.py
+++ work in progress.py
@@ -6,6 +6,8 @@ from ChARActer.chARActer import *
 
 from MOBS.mobsC import *
 
+from Roms.room_for_rooms import *
+
 def starting():
     beginning = input("Type start_adventure when are ready to begin ")
     if beginning == "start_adventure":
@@ -14,6 +16,10 @@ def starting():
         print("Try again when you are ready")
         beginning = input("Type start_adventure when are ready to begin ")
 
+print("This is be the best RPG game you will play in this period. So the story is probs a fantsy world and your character is trash. The whole game is about trying to level up from adv rank F and also not to rage quit the game because of some great devs :). Anyways the game begins with MC becoming a adventurer but adventures start at rank F, how orignial. MC is a failure. But, from exploring life and beating monster, and also getting beat by the monster, the MC can level up and not be failure(L). ")
+
+
 ara_ara = input("WHaT do you wAnt to nAme your chARActer?") 
+
 
 
