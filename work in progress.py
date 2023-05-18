a789e1badb40e2beafd3deb1afc9418a56e9d630
AnnaLi3 <124058687+AnnaLi3@users.noreply.github.com>
2023-05-18 08:27:45 -0400

    :)

--- work in progress.py
+++ work in progress.py
@@ -6,6 +6,10 @@ from ChARActer.chARActer import *
 
 from MOBS.mobsC import *
 
+ara_ara = input("What do you want to name your character?") 
+
+print("This a cool game set in a fantasy world. ")
+
 def starting():
     beginning = input("Type start_adventure when are ready to begin ")
     if beginning == "start_adventure":
@@ -13,7 +17,4 @@ def starting():
     else:
         print("Try again when you are ready")
         beginning = input("Type start_adventure when are ready to begin ")
-
-print("This a cool game set in a fantasy world. ")
-
 
