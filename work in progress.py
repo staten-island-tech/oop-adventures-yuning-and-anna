d75f26af444fd2a34aec940e8ed31cabb3452191
ierhehe <ierhehe@gmail.com>
2023-05-18 08:39:29 -0400

    dushgfkf

--- work in progress.py
+++ work in progress.py
@@ -16,9 +16,9 @@ def starting():
 
 print("This is be the best RPG game you will play in this period. So the story is probs a fantsy world and your character is trash. The whole game is about trying to level up from adv rank F and also not to rage quit the game because of some great devs :). Anyways the game begins with MC becoming a adventurer but adventures start at rank F, how orignial. MC is a failure. But, from exploring life and beating monster, and also getting beat by the monster, the MC can level up and not be failure(L). ")
 
-
 ara_ara = input("WHaT do you wAnt to nAme your chARActer?") 
 
 print
+
 
 
