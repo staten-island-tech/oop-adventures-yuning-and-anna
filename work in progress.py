b9809fc9da423b0e959667aa37ad95fca41783cb
ierhehe <ierhehe@gmail.com>
2023-05-22 08:32:05 -0400

    srdsfgdf

--- work in progress.py
+++ work in progress.py
@@ -17,7 +17,10 @@ def starting():
 print("This is the best RPG game you will play in this period. So the story is probs a fantasy world and your character is trash. The whole game is about trying to level up from adventure rank F and also not to rage quit the game because of some great devs :). Anyways the game begins with MC becoming an adventurer but adventures start at rank F, how original. MC is a failure. But, from exploring life and beating monsters, and also getting beat by monsters, the MC can level up and not be a failure(L). ")
 
 
-ara_ara = input("WHaT do you wAnt to nAme your chARActer?") 
+ara_ara = input("What do you want to name your character?") 
 
 starting()
 
+print(room_1)
+
+c
