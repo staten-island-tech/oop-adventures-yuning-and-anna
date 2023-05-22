663f843e91e8508939a078456f68568a22c262f9
ierhehe <ierhehe@gmail.com>
2023-05-22 08:38:57 -0400

    hfgf

---
+++ Crying.py
@@ -0,0 +1,12 @@
+rooms = {
+    'room_1':{'south': 'room_2', 'east': 'room_3'},
+    'room_2':{'south': 'gambling_2'},
+    'room_3':{'north': 'room_4', 'east': 'gambling'},
+    'Room_4':{'west' : 'room_1'},
+    'room_5':{'east': 'room_7'},
+    'room_6':{'north': 'gambling_1', 'south': 'room_9', 'east': 'CRY_PEASANTS'},
+    'room_7':{'south': 'CRY_PEASANTS'},
+    'room_8':{'east': 'room_9'},
+    'room_9':{'east': 'room_10'},
+    'room_10':{'west': 'room_9'}
+}
