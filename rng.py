1d14c2870e76d9ed8846dc5c4ae2c008e2f5e31d
AnnaLi3 <124058687+AnnaLi3@users.noreply.github.com>
2023-05-23 08:37:00 -0400

    :(

--- rng.py
+++ rng.py
@@ -8,10 +8,9 @@ print(freebies)
 if freebies == 1:
     if "katana" in Item:
         freebies = random.randint(0, 6)
-    elif weapon_damage > 85:
-        print("You got a Katana! Yay! But this does less damage than you current weapon so it would not be applied! So sorry! Good luck on the journey!!!!")
     else:
         weapon_damage = 85
+        print(weapon_damage)
         Item.append("katana")
         print(Item)
         
