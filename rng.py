990845f1a70de043cda8675479f2bf45d0c056e1
AnnaLi3 <124058687+AnnaLi3@users.noreply.github.com>
2023-05-23 08:33:18 -0400

    :(

--- rng.py
+++ rng.py
@@ -1,5 +1,18 @@
 import random
+from Skills.movves import *
+from ChARActer.chARActer import *
+Item = []
 
-freebies = random.randint(0, 6)
+freebies = random.randint(0, 1)
+print(freebies)
 if freebies == 1:
-dsadsa
+    if "katana" in Item:
+        freebies = random.randint(0, 6)
+    elif weapon_damage > 85:
+        print("You got a Katana! Yay! But this does less damage than you current weapon so it would not be applied! So sorry! Good luck on the journey!!!!")
+    else:
+        weapon_damage = 85
+        Item.append("katana")
+        print(Item)
+        
+
