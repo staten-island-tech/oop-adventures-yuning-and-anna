b8f5e5f77bd8ae337ec1e59fd77b9f4d79e16e6c
AnnaLi3 <124058687+AnnaLi3@users.noreply.github.com>
2023-05-24 08:39:29 -0400

    :)

--- ChARActer/chARActer.py
+++ ChARActer/chARActer.py
@@ -1,7 +1,8 @@
-
-
+from freebies import *
+damage = weapon_damage
 class MC():
-    def __init__(self, health, defense):
+    def __init__(self, health, defense, damage):
         self.health = health 
         self.defense = defense
-ara_ara = MC(300, 0)
+        self.damage = damage
+ara_ara = MC(300, 0, damage)
