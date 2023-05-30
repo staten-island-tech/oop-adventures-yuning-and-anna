2e55938108e45b3e645d91ffa766c1a6258c2d0a
AnnaLi3 <124058687+AnnaLi3@users.noreply.github.com>
2023-05-30 08:32:31 -0400

    dfsa

--- work in progress.py
+++ work in progress.py
@@ -6,13 +6,16 @@ from ChARActer.chARActer import *
 
 from MOBS.mobsC import *
 
+from Crying import *
+alreadyfoughten = ["room1"]
+
 def starting():
-    beginning = input("Type start_adventure when are ready to begin ")
-    if beginning == "start_adventure":
+    beginning = input("Type start when are ready to begin ")
+    if beginning == "start":
         print(intro_room_beginning)
     else:
         print("Try again when you are ready")
-        beginning = input("Type start_adventure when are ready to begin ")
+        beginning = input("Type start when are ready to begin ")
 
 print("This is the best RPG game you will play in this period. So the story is probs a fantasy world and your character is trash. The whole game is about trying to level up from adventure rank F and also not to rage quit the game because of some great devs :). Anyways the game begins with MC becoming an adventurer but adventures start at rank F, how original. MC is a failure. But, from exploring life and beating monsters, and also getting beat by monsters, the MC can level up and not be a failure(L). ")
 
@@ -21,6 +24,10 @@ ara_ara = input("What do you want to name your character?")
 
 starting()
 
-print(room_1)
+CurrentRoom = room_1
 
-c
+if CurrentRoom == room_1:
+    if "room1" in alreadyfoughten:
+        user_move_room_1()
+    else: 
+        print("hi")
